1589260615f6b5d75fd6d4f00a57310d9ad31f5f
HuhuliaDiana <dianahuhulia@gmail.com>
2021-06-22 02:03:51 +0300

    new commiitttttttttt

--- frontend/src/pages/LogIn.vue
+++ frontend/src/pages/LogIn.vue
@@ -1,29 +1,73 @@
-<template>
-  <div class="q-pa-md" style="max-width: 400px">
-    <q-form @submit="onLoggedIn" class="q-gutter-md">
-      <q-input
-        filled
-        v-model="email"
-        label="Your email *"
-        hint="Email"
-        lazy-rules
-        :rules="[(val) => (val && val.length > 0) || 'Please type something']"
-      />
-
-      <q-input
-        filled
-        v-model="password"
-        type="password"
-        label="Your password *"
-        hint="Password"
-        lazy-rules
-        :rules="[(val) => (val && val.length > 0) || 'Please type something']"
-      />
-
-      <div>
-        <q-btn label="Log in" type="submit" color="primary" />
+<template style="">
+  <div
+    class="page"
+    style="
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      height: 100vh;
+    "
+  >
+    <div class="flex form" style="width: 70%; justify-content: space-between;height:900px">
+      <div
+        style="
+          width: 40%;
+          background-color: #26a69b;
+          color: white;
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          font-style: italic;
+          font-size: 400%;
+        "
+      >
+        WiseCell
       </div>
-    </q-form>
+      <div class="login" style="width: 50%;">
+        <div
+          style="
+            padding: 50px;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+          "
+        >
+          <q-form
+            @submit="onLoggedIn"
+            class="q-gutter-md flex flex-center"
+            style="width: 100%;border:1px solid"
+          >
+            <q-input
+              filled
+              v-model="email"
+              label="Email *"
+              hint="Email"
+              style="width: 80%"
+              lazy-rules
+              :rules="[
+                (val) => (val && val.length > 0) || 'Please type something',
+              ]"
+            />
+            <q-input
+              filled
+              v-model="password"
+              style="width: 80%"
+              type="password"
+              label="Parola *"
+              hint="Parola"
+              lazy-rules
+              :rules="[
+                (val) => (val && val.length > 0) || 'Please type something',
+              ]"
+            />
+
+            <div style="display: flex; justify-content: center">
+              <q-btn label="Login" type="submit" color="secondary" />
+            </div>
+          </q-form>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -78,5 +122,17 @@ export default {
 };
 </script>
 
-<style>
+<style scoped>
+.form {
+  box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
+}
+.page {
+  background: -webkit-linear-gradient(
+    left,
+    #26a69b,
+    #26a69b 40%,
+    #f0f0f0 0%,
+    #f0f0f0
+  );
+}
 </style>
