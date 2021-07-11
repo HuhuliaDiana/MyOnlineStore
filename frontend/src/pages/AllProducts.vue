49c0c0b3bbc7f2559adc957d985cefa21c7b685a
HuhuliaDiana <dianahuhulia@gmail.com>
2021-07-11 14:42:54 +0300

    last commit

--- frontend/src/pages/AllProducts.vue
+++ frontend/src/pages/AllProducts.vue
@@ -249,6 +249,16 @@
 <script>
 import axios from "../boot/axios";
 import ToolbarAdmin from "../components/ToolbarAdmin.vue";
+const multer=require('multer')
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, '../frontend/public/photos')
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname)
+  }
+})
+const upload = multer({ storage: storage })//limits:{filesize: ...}
 
 export default {
   components: {
